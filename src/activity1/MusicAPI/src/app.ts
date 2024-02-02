950142ba5fa2486547d2ea690ea1c9c653a5578f
Owen Lindsey <Owen.linsey03@Outlook.com>
2024-02-02 11:25:06 -0700

    Set up TypeScript compiler configuration and added dotenv for environment variable management.

--- src/activity1/MusicAPI/src/app.ts
+++ src/activity1/MusicAPI/src/app.ts
@@ -1,11 +1,18 @@
+// To use environment variables
+import dotenv from 'dotenv';
+dotenv.config({ path: 'MusicAPI/.env' });
+
+// Print the environment variable to the console
+console.log(process.env.GREETING);  // This should print: Hello from the environment file. Be kind to the environment!
+
 // Importing the express module and specific types from 'express'
 import express, { Request, Response } from 'express';
 
 // Creating an instance of express
 const app = express();
 
-// Defining the port number on which the server will listen
-const port = 9991;
+// The port number will now be taken from the environment variables if defined
+const port = process.env.PORT || 9991; // Fallback to 9991 if the environment variable is not defined
 
 // Handling GET requests to the root ('/') URL
 // req represents the request object, res represents the response object
@@ -16,7 +23,6 @@ app.get('/', (req: Request, res: Response) => {
 
 // Starting the server on the specified port
 // The listen method takes the port number and an optional callback function
-
 app.listen(port, () => {
     // This callback function is executed once the server starts listening
     // Logging the URL where the server is listening
